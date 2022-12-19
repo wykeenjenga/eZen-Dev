44b3cba72e43c8e6f605ce616bd4f4e3ffd8c18e
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2022-12-19 10:14:49 +0300

    🔥

--- eZen Dev/Business/API Gateway/APAPIGateway.swift
+++ eZen Dev/Business/API Gateway/APAPIGateway.swift
@@ -7,20 +7,16 @@
 //
 
 import Foundation
-import Firebase
 
 class APAPIGateway {
     
     private static let _door = APAPIGateway.init()
-    private var firebase: APFirebase!
     
     @discardableResult class func door() -> APAPIGateway {
         return self._door
     }
     
     private init() {
-        firebase = APFirebase()
-        configureFirebase()
     }
     
     var currentToken: String? {
@@ -29,29 +25,7 @@ class APAPIGateway {
         }
     }
     
-    private func configureFirebase() {
-        firebase.configure()
-    }
-    
-    func signInUser(with email: String, password: String, completion: @escaping (Result<Bool,WrappedError>)->()) {
-
-        firebase.signIn(with: email, password: password) { result in
-            switch result {
-            case .success(_):
-                completion(result)
-            case .failure(_):
-                completion(result)
-            }
-        }
-    }
-    
-    func signUpUser(with email: String, password: String, completion: @escaping (Result<Bool,WrappedError>)->()) {
-        firebase.signUp(with: email, password: password) { (result) in
-            completion(result)
-        }
-    }
-    
     func getProductDetail(barCode: String, completion: @escaping (APProductsModel, Error?) -> Void){
-        firebase.getProductDetail(barCode: barCode, completion: completion)
+        //firebase.getProductDetail(barCode: barCode, completion: completion)
     }
 }
