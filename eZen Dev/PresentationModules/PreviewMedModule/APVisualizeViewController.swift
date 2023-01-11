54ead85759528fd20263f4247e9b3d58c68bc873
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2023-01-11 10:31:03 +0300

    ccc

--- eZen Dev/PresentationModules/PreviewMedModule/APVisualizeViewController.swift
+++ eZen Dev/PresentationModules/PreviewMedModule/APVisualizeViewController.swift
@@ -195,14 +195,18 @@ class APVisualizeViewController: BaseViewController {
                         
                         for sentence in self.words{
                             
-                            let start = word.start
-                            let end = word.end
-                            let punctuatedWord = word.punctuatedWord
+                            let start = sentence.start
+                            let end = sentence.end
+                            let punctuatedWord = sentence.transcript
 
                             let range = start...end
 
                             if range.contains(time){
-                                
+                                //self.transcriptionLbl.isHidden = false
+                                self.transcriptionLbl.text = punctuatedWord
+                                print(">>>>>>>>>.\(punctuatedWord)....\(start)......\(end)")
+                            }else{
+                                //self.transcriptionLbl.isHidden = true
                             }
                             
                         }
